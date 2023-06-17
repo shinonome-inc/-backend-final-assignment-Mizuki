d75f87d30725bfbb894899acb7c43ff20ff55608
Lai <lokutouzan@gmail.com>
2023-06-17 12:10:19 +0900

    ログイン・ログアウト機能修正

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,9 @@ app_name = "accounts"
 
 urlpatterns = [
     path("signup/", views.SignupView.as_view(), name="signup"),
+
+
+
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('<str:username>/', views.UserProfileView.as_view(), name='user_profile'),
