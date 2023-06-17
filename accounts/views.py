816ec12f128181a84ad691e1ab42a6b81edbec01
Lai <lokutouzan@gmail.com>
2023-06-17 12:06:01 +0900

    ログイン・ログアウト機能修正

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from django.views import View
-from django.contrib.auth.views import LoginView, LogoutView
-
-from .forms import LoginForm
-from .forms import SignupForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import CreateView
+
+from .forms import LoginForm, SignupForm
 
 
 class SignupView(CreateView):
