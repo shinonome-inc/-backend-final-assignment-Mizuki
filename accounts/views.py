f585ac982f5f31ca36df5217148f8e43d9591fc5
Lai <lokutouzan@gmail.com>
2023-06-18 15:10:44 +0900

    コメント修正

--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView
 
-from .models import User
 from .forms import SignupForm
+from .models import User
 
 
 class SignupView(CreateView):
