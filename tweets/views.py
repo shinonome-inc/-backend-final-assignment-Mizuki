51820accf5e58837a7530241a4a0712e6b0ddbc1
Lai <lokutouzan@gmail.com>
2024-01-03 22:43:13 +0900

    Home画面いいね実装

--- tweets/views.py
+++ tweets/views.py
@@ -1,24 +1,31 @@
 # from django.shortcuts import render
 from typing import Any
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db.models.query import QuerySet
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, View
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
+from django.db.models import Prefetch, Count
 
 from .models import Tweet, Like
 
-
+#ListViewはquerysetで取得する
+#queryset使わずcontextで渡すならTemplateViewでいい
 class HomeView(LoginRequiredMixin, ListView):
     model = Tweet
     template_name = "tweets/home.html"
     context_object_name = "tweet_list"
-    queryset = Tweet.objects.select_related("user")
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["liked_list"] = Like.objects.filter(user=self.request.user)
-        return context
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Tweet.objects.select_related("user").prefetch_related(
+            Prefetch("likes",
+                     queryset=Like.objects.filter(user=user),
+                     to_attr="is_liked"
+            )
+        ).annotate(liked_count=Count("likes"))
+        return queryset  
 
 
 class TweetCreateView(LoginRequiredMixin, CreateView):
@@ -55,12 +62,12 @@ class LikeView(LoginRequiredMixin, View):
         tweet_id = self.kwargs["pk"]
         tweet = get_object_or_404(Tweet, id=tweet_id)
         unlike_url = reverse("tweets:unlike", kwargs={"pk": tweet_id})
-        is_liked = False
+        is_liked = True
 
         Like.objects.get_or_create(user=user, tweet=tweet)
-        likes_count = Like.objects.count()
+        likes_count = Like.objects.filter(tweet=tweet).count()
         context = {
-            "likes_count": likes_count,
+            "liked_count": likes_count,
             "is_liked": is_liked,
             "tweet_id": tweet_id,
             "unlike_url": unlike_url,
@@ -75,13 +82,13 @@ class UnlikeView(LoginRequiredMixin, View):
         tweet_id = self.kwargs["pk"]
         tweet = get_object_or_404(Tweet, id=tweet_id)
         like_url = reverse("tweets:like", kwargs={"pk": tweet_id})
-        is_liked = True
+        is_liked = False
 
         like = Like.objects.filter(user=user, tweet=tweet)
         like.delete()
-        likes_count = Like.objects.count()
+        likes_count = Like.objects.filter(tweet=tweet).count()
         context = {
-            "likes_count": likes_count,
+            "liked_count": likes_count,
             "is_liked": is_liked,
             "tweet_id": tweet_id,
             "like_url": like_url,
